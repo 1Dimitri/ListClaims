06b6f7fd0045883814983e07c9526c31119588e7
1Dimitri <git.dimitri@janczak.net>
2015-08-15 11:17:44 +0200

    Simple Claims list dumper
    Simple ASP.NET MVC 5 Bootstrap layout application which dumps
    CLaimsPrincipal
    The WEb.Config has everwthing to connect to an Active Directory
    Federation Services Farm
    Used to debug ADFS / Claims configuration issues

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IdentityModel;
+using System.Security.Claims;
+using System.Threading;
+
+namespace ListClaims.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            ClaimsPrincipal userClaims = Thread.CurrentPrincipal as ClaimsPrincipal;
+            // ViewBag.Claims = userClaims;
+            return View(userClaims);
+
+        }
+/* public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+ * */
+    }
+ 
+}
